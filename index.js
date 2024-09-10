117483175f204746f62ec9b4082fa8d955c3e1ec
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-09-10 19:09:04 +0300

    finish

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-// Знайти максимальний елемент масиву
-
-const array = [1,2,3,5,4,9,8,7];
-
-function maximum (array){
-    return Math.max(...array)
-};
-
-console.log(maximum(array)); 
+import {maximum} from "./idx";
